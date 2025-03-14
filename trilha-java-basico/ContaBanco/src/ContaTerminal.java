5eb28664c7c57cc92936880af8032549aeff62ee
Yuri Alcantara <yuripeleskei@gmail.com>
2025-03-14 15:09:35 -0300

    Segundo Commit

---
+++ trilha-java-basico/ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,13 @@
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        /** 
+        TODO:Conhecer e importar a class Scanner
+        Exibir a mensagem para nossos usuários
+        Obter pela class Scanner os valores digitados pelos usuários
+        
+        Exibir a mensagem de conta criada
+        "Olá [Nome Cliente], obrigado por criar uma conta em nosso banco, sua agência é [Agencia], 
+        conta [Numero] e seu saldo [Saldo] já está disponível para saque".
+        **/
+    }
+}
